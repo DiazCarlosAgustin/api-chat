b01a9d4f4ce3fa3898ffb6de531b0b8a9f1e3b3b
Diaz Carlos <carlos.agustin.diaz1998@gmail.com>
2022-04-11 19:39:26 -0300

    Controlador de imagenes

---
+++ controllers/image.controller.js
@@ -0,0 +1,26 @@
+const {v4: uuidv4 } = require("uuid")
+exports.uploadImage = async function(files){
+    let img, uploadPath, msg;
+
+    //asignacion de valores 
+	img = files.image; //tomo la imagen del files en req
+
+	const extension = img.name.split('.')[1]; //extraigo la extension del archivo
+	
+	const imgName = uuidv4() + '.' + extension //creo un nombre unico y le agrego la extension 
+
+	uploadPath = `./public/img/${imgName}`; //le digo donde lo voy a guardar la img
+	
+    await img.mv(uploadPath, async (err) =>{
+        if(err){
+            return {
+                error: true, mensaje: err
+            }
+        }
+    })
+    return {
+        error: false, 
+        mensaje: 'File uploaded',
+        name: imgName
+    }
+}
