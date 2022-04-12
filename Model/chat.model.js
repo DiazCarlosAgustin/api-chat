d354e55f1212cb37a2f5ba56c4d8a2bd59c11888
Diaz, Carlos Agustin <47919332+DiazCarlosAgustin@users.noreply.github.com>
2022-04-12 15:27:50 -0300

    add timestamps chat and user

--- Model/chat.model.js
+++ Model/chat.model.js
@@ -1,13 +1,35 @@
 const mongoose = require("mongoose");
 
+// const chatSchema = new mongoose.Schema({
+// 	users: {
+// 		type: Array,
+// 	},
+// 	chat: {
+// 		type: Array,
+// 	},
+// });
+
 const chatSchema = new mongoose.Schema({
 	users: {
-		type: Array,
+		type: Array
 	},
-	chat: {
-		type: Array,
-	},
-});
+	chat: [
+		{
+			from: {
+				type: String
+			},
+			to: {
+				type: String
+			},
+			message: {
+				type: String
+			},
+			createdAt:{
+				type: Date
+			}
+		}
+	]
+},{ timestamps: true })
 
 const chatModel = mongoose.model("chat", chatSchema);
 
--- Model/user.model.js
+++ Model/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
 	image: {
 		type: String,
 	}
-});
+},{ timestamps: true });
 
 const User = mongoose.model("User", userSchema);
 
